1bf3ef986251559bf33814168beb1457a1cec14a
Jorge Pablo <pablojorgel01@gmail.com>
2023-03-09 22:35:24 -0300

    hasta array de productos agregados

--- JS/main.js
+++ JS/main.js
@@ -1,186 +1,215 @@
 // PRODUCTOS
 const productos = [
-    // Abrigos
+    // Asadores
     {
-        id: "abrigo-01",
-        titulo: "Abrigo 01",
-        imagen: "./img/abrigos/01.jpg",
+        id: "asador-01",
+        titulo: "Asador chico",
+        imagen: "./imagenes/asador _chico.jpg",
         categoria: {
-            nombre: "Abrigos",
-            id: "abrigos"
+            nombre: "Parrillas",
+            id: "parrillas"
+        },
+        precio: 3500
+    },
+    {
+        id: "asador-02",
+        titulo: "Chulengo",
+        imagen: "./imagenes/chulengo.jpg",
+        categoria: {
+            nombre: "Parrillas",
+            id: "parrillas"
+        },
+        precio: 3500
+    },
+    {
+        id: "asador-03",
+        titulo: "Parrilla Boca Jr",
+        imagen: "./imagenes/parrilla_boca_jrs.jpg",
+        categoria: {
+            nombre: "Parrillas",
+            id: "parrillas"
         },
         precio: 1000
     },
     {
-        id: "abrigo-02",
-        titulo: "Abrigo 02",
-        imagen: "./img/abrigos/02.jpg",
+        id: "asador-04",
+        titulo: "Parrila River Plate",
+        imagen: "./imagenes/parrilla_river.jpg",
         categoria: {
-            nombre: "Abrigos",
-            id: "abrigos"
+            nombre: "Parrillas",
+            id: "parrillas"
         },
-        precio: 1000
+        precio: 3500
     },
     {
-        id: "abrigo-03",
-        titulo: "Abrigo 03",
-        imagen: "./img/abrigos/03.jpg",
+        id: "asador-05",
+        titulo: "Parrilla portatil",
+        imagen: "./imagenes/parrilla_portatil_1.jpg",
         categoria: {
-            nombre: "Abrigos",
-            id: "abrigos"
+            nombre: "Parrillas",
+            id: "parrillas"
         },
-        precio: 1000
+        precio: 3500
     },
     {
-        id: "abrigo-04",
-        titulo: "Abrigo 04",
-        imagen: "./img/abrigos/04.jpg",
+        id: "asador-06",
+        titulo: "Parrilla clasica",
+        imagen: "./imagenes/prrilla_clasica.jpg",
         categoria: {
-            nombre: "Abrigos",
-            id: "abrigos"
+            nombre: "Parrillas",
+            id: "parrillas"
         },
-        precio: 1000
+        precio: 3500
+    },
+    // Discos
+    {
+        id: "discos-01",
+        titulo: "Disco con tapa",
+        imagen: "./imagenes/disco _con _tapa.jpg",
+        categoria: {
+            nombre: "Discos",
+            id: "discos"
+        },
+        precio: 2500
     },
     {
-        id: "abrigo-05",
-        titulo: "Abrigo 05",
-        imagen: "./img/abrigos/05.jpg",
+        id: "discos-02",
+        titulo: "Disco parrilla",
+        imagen: "./imagenes/disco_con_parrilla.jpg",
         categoria: {
-            nombre: "Abrigos",
-            id: "abrigos"
+            nombre: "Discos",
+            id: "discos"
         },
-        precio: 1000
-    },
-    // Camisetas
-    {
-        id: "camiseta-01",
-        titulo: "Camiseta 01",
-        imagen: "./img/camisetas/01.jpg",
-        categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
-        },
-        precio: 1000
+        precio: 2500
     },
     {
-        id: "camiseta-02",
-        titulo: "Camiseta 02",
-        imagen: "./img/camisetas/02.jpg",
+        id: "discos-03",
+        titulo: "Disco con patas",
+        imagen: "./imagenes/disco con patas.jpg",
         categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
+            nombre: "Discos",
+            id: "discos"
         },
-        precio: 1000
+        precio: 2500
+    },
+    //Accesorios
+    {
+        id: "accesorios-1",
+        titulo: "Brasero decorado",
+        imagen: "./imagenes/brasero_decorado.jpg",
+        categoria: {
+            nombre: "Accesorios",
+            id: "accesorios"
+        },
+        precio: 1500
     },
     {
-        id: "camiseta-03",
-        titulo: "Camiseta 03",
-        imagen: "./img/camisetas/03.jpg",
+        id: "accesorios-2",
+        titulo: "Brasero de mesa",
+        imagen: "./imagenes/brasero_mesa.jpg",
         categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
+            nombre: "Accesorios",
+            id: "accesorios"
         },
-        precio: 1000
+        precio: 1500
     },
     {
-        id: "camiseta-04",
-        titulo: "Camiseta 04",
-        imagen: "./img/camisetas/04.jpg",
+        id: "accesorios-3",
+        titulo: "Jaula parrilla",
+        imagen: "./imagenes/jaula_parrrilla.jpg",
         categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
+            nombre: "Accesorios",
+            id: "accesorios"
         },
-        precio: 1000
+        precio: 1500
     },
     {
-        id: "camiseta-05",
-        titulo: "Camiseta 05",
-        imagen: "./img/camisetas/05.jpg",
+        id: "accesorios-4",
+        titulo: "Kit asador",
+        imagen: "./imagenes/kit_asador.jpg",
         categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
+            nombre: "Accesorios",
+            id: "accesorios"
         },
-        precio: 1000
+        precio: 150
     },
     {
-        id: "camiseta-06",
-        titulo: "Camiseta 06",
-        imagen: "./img/camisetas/06.jpg",
+        id: "accesorios-5",
+        titulo: "Set cubiertos",
+        imagen: "./imagenes/set_cubiertos _asado.jpg",
         categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
+            nombre: "Accesorios",
+            id: "accesorios"
         },
-        precio: 1000
+        precio: 150
     },
-    {
-        id: "camiseta-07",
-        titulo: "Camiseta 07",
-        imagen: "./img/camisetas/07.jpg",
-        categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
-        },
-        precio: 1000
-    },
-    {
-        id: "camiseta-08",
-        titulo: "Camiseta 08",
-        imagen: "./img/camisetas/08.jpg",
-        categoria: {
-            nombre: "Camisetas",
-            id: "camisetas"
-        },
-        precio: 1000
-    },
-    // Pantalones
-    {
-        id: "pantalon-01",
-        titulo: "Pantalón 01",
-        imagen: "./img/pantalones/01.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
-    },
-    {
-        id: "pantalon-02",
-        titulo: "Pantalón 02",
-        imagen: "./img/pantalones/02.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
-    },
-    {
-        id: "pantalon-03",
-        titulo: "Pantalón 03",
-        imagen: "./img/pantalones/03.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
-    },
-    {
-        id: "pantalon-04",
-        titulo: "Pantalón 04",
-        imagen: "./img/pantalones/04.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
-    },
-    {
-        id: "pantalon-05",
-        titulo: "Pantalón 05",
-        imagen: "./img/pantalones/05.jpg",
-        categoria: {
-            nombre: "Pantalones",
-            id: "pantalones"
-        },
-        precio: 1000
-    }
 ];
+
+const contenedorProductos = document.querySelector("#contenedor_productos");
+const botonesCategorias= document.querySelectorAll(".boton-categoria"); 
+const tituloPrincipal= document.querySelector("#titulo_principal");
+let botonesAgregar= document.querySelectorAll(".producto_agregar");
+
+function cargarProductos(productosElegidos){
+
+    contenedorProductos.innerHTML="";
+
+    productosElegidos.forEach( producto =>{
+
+        const div=document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML=`<img class="producto_imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                 
+        <div class="producto_detalles">
+            <h3 class="producto_titulo">${producto.titulo}</h3>
+             <p class="producto_precio"><span>$</span>${producto.precio}</p>
+            <button class="producto_agregar" id="${producto.id}">Agregar</button>
+        </div>`; 
+        contenedorProductos.append(div);
+
+    })
+
+    actualizarBotonesAgregar();
+ }; 
+
+ cargarProductos(productos);
+
+
+botonesCategorias.forEach(boton =>{
+    boton.addEventListener("click",(e) =>{
+
+        botonesCategorias.forEach(boton=> boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if(e.currentTarget.id != "todos"){
+
+            const productoCategoria= productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+        const productosBoton = productos.filter(producto =>producto.categoria.id=== e.currentTarget.id);
+        cargarProductos(productosBoton);
+    }else{
+        cargarProductos(productos);
+        tituloPrincipal.innerText = "Todos los productos";
+    }
+    } )
+});
+
+function actualizarBotonesAgregar(){
+    botonesAgregar= document.querySelectorAll(".producto_agregar");
+
+     botonesAgregar.forEach(boton =>{
+          boton.addEventListener("click", agregarAlCacrrito);
+    });
+
+};
+          
+ const ProductosEnCacrrito=[];
+
+function agregarAlCacrrito(e){
+     const idBoton= e.currentTarget.id;
+      const productoAgregado= productos.find(producto=> producto.id === idBoton);   
+      ProductosEnCacrrito.push(productoAgregado); 
+       console.log(ProductosEnCacrrito);
+}
+// 1:31:35
